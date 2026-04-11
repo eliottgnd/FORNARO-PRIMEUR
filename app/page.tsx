c1acf2bb557065e0fdb934b88a989f8dd4b78ddf
Eliott Guinaudie <eliott.guinaudie64@gmail.com>
2026-04-11 11:13:16 +0200

    Modification sous titre hero

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
           </h1>
 
           <p ref={heroSubRef} className="text-creme/60 text-[14px] md:text-[15px] leading-relaxed max-w-sm mb-8 md:mb-12 opacity-0">
-            Fruits, légumes et produits frais sélectionnés chaque jour et livrés
+            Fruits, légumes frais sélectionnés chaque jour et livrés
             à domicile ou lieu de votre choix (voir zones desservies) par votre primeur.
           </p>
 
